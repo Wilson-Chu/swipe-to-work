0511e1d26c4e3526f50ac0473e1203cbd392866a
Wilson Chu <wilson1.chu@gmail.com>
2023-11-26 15:54:26 -0500

    setup basic routes and setup DB

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import DataList from "./components/DataList";
-import Status from "./components/Status";
 import useApplicationData from "./hooks/useApplicationData";
 import "./App.scss";
 import Home from "./pages/Home";
+import Preferences from "./pages/Preferences";
+import Saved from "./pages/Saved";
+import Navbar from "./components/Navbar";
+import data from "./mockdata/data";
 
 export default function App() {
   return (
     <div className="App">
       <Router>
-        <h1>Swipe to Work</h1>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          {/* <Route path="/account" element={<Account />} /> */}
+          <Route path="/preferences" element={<Preferences />} />
+          <Route path="/saved" element={<Saved />} />
         </Routes>
       </Router>
     </div>
--- server/database/00-schema.sql
+++ server/database/00-schema.sql
@@ -1,6 +1,41 @@
-DROP TABLE IF EXISTS items CASCADE;
+DROP TABLE IF EXISTS users, preferences, saved_jobs CASCADE;
 
-CREATE TABLE items (
+CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(32) NOT NULL
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE preferences (
+  id SERIAL PRIMARY KEY NOT NULL,
+  job_title VARCHAR(255),
+  company TEXT DEFAULT NULL,
+  city VARCHAR (255),
+  province VARCHAR (255),
+  min_salary INTEGER DEFAULT NULL,
+  max_salary INTEGER DEFAULT NULL,
+  job_type VARCHAR(255) DEFAULT NULL,
+  is_remote BOOLEAN DEFAULT FALSE,
+  no_experience_required BOOLEAN DEFAULT TRUE,
+  min_education_level VARCHAR(255) DEFAULT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE saved_jobs (
+  id SERIAL PRIMARY KEY NOT NULL,
+  job_posting_id VARCHAR(255),
+  applied BOOLEAN DEFAULT NULL,
+  company TEXT,
+  job_title VARCHAR(255),
+  city VARCHAR (255),
+  province VARCHAR (255),
+  min_salary INTEGER,
+  max_salary INTEGER,
+  job_description TEXT,
+  job_type VARCHAR(255),
+  is_remote BOOLEAN DEFAULT FALSE,
+  benefits TEXT DEFAULT NULL,
+  posted_at DATE,
+  website TEXT DEFAULT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
