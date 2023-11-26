42a697f58d850bf6960a98202641c54d26cbf423
Wilson Chu <wilson1.chu@gmail.com>
2023-11-26 05:54:00 -0500

    update sass dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DataList from "./components/DataList";
 import Status from "./components/Status";
 import useApplicationData from "./hooks/useApplicationData";
@@ -6,17 +7,14 @@ import "./App.scss";
 import Home from "./pages/Home";
 
 export default function App() {
-
   return (
     <div className="App">
-      
-
       <Router>
-      <h1>Swipe to Work</h1>
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
-          </Router>
+        <h1>Swipe to Work</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
