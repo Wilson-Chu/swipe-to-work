521476825c015147a1c8f90f58a4f1fe0e56e2c0
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-11-27 17:19:16 -0500

    create state and functions

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -8,18 +8,28 @@ import { useState } from 'react';
 
 const Home = function(props) {
 
+  const [job, setJob] = useState(0)
   const [modal, setModal] = useState(false)
 
   const openModal = function () {
     setModal(true)
   }
+
+  const closeModal = function () {
+    setModal(false)
+  }
+
+  const nextJob = function() {
+    setJob(prev =>  prev + 1)
+  }
+
   return (
       <div>
-        {data.data[2].employer_logo && <div><img className="main-image" src={data.data[2].employer_logo} alt="employer logo" onClick={() => openModal()}/></div>}
-        {!data.data[2].employer_logo && <div><img className="main-image" src="https://redlakejobs.ca/wp-content/uploads/2020/10/employment.jpg" alt="jobs" onClick={() => openModal()}/></div>}
-        <MainJobPoints data={data.data} />
-        <ActionButtons />
-        {!!modal && <JobDetailsModal data={data.data}/>}
+        {data.data[job].employer_logo && <div><img className="main-image" src={data.data[2].employer_logo} alt="employer logo" onClick={() => openModal()}/></div>}
+        {!data.data[job].employer_logo && <div><img className="main-image" src="https://redlakejobs.ca/wp-content/uploads/2020/10/employment.jpg" alt="jobs" onClick={() => openModal()}/></div>}
+        <MainJobPoints data={data.data} job={job}/>
+        <ActionButtons nextJob={nextJob}/>
+        {!!modal && <JobDetailsModal data={data.data} closeModal={closeModal} job={job}/>}
       </div>
   );
 }
