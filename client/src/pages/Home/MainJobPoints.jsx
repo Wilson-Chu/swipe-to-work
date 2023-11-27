be29e02f17e6433a7ec3b55ac4c97b21cf69999d
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-11-27 17:18:14 -0500

    refactor to use job state instead of hard coding the index number

--- client/src/pages/Home/MainJobPoints.jsx
+++ client/src/pages/Home/MainJobPoints.jsx
@@ -4,20 +4,20 @@ const MainJobPoints = function (props) {
   return (
     <div className='main-points-container'>
       <h2>
-        {props.data[2].job_title}
+        {props.data[props.job].job_title}
       </h2>
       <ul className="main-job-points">
         <li>
           <i className="fa-solid fa-briefcase"></i>
-          <p>{props.data[2].employer_name}</p>
+          <p>{props.data[props.job].employer_name}</p>
         </li>
         <li>
           <i className="fa-solid fa-location-dot"></i>
-          <p>{props.data[2].job_city}</p>
+          <p>{props.data[props.job].job_city}</p>
         </li>
         <li>
           <i className="fa-regular fa-clock"></i>
-          <p>{props.data[2].job_employment_type}</p>
+          <p>{props.data[props.job].job_employment_type}</p>
         </li>
       </ul>
     </div>
