52330df3454d5a3a5b90877e275e8b00c24c02e9
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-11-27 12:55:16 -0500

    button components for Home page

---
+++ client/src/pages/Home/ActionButtons.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ActionButtons = function () {
+  return (
+    <div className="action-buttons">
+      <button type="button">
+        Pass
+      </button>
+      <button type="button">
+        Random!
+      </button>
+      <button type="button">
+        Save
+      </button>
+    </div>
+  )
+}
+
+export default ActionButtons;
