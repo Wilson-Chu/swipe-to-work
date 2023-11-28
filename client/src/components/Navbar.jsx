e81dd2947a85650076693a47b7f25c0589b9c838
lorraineC26 <lorraine.quinyee@gmail.com>
2023-11-28 02:40:23 -0500

    add nav icons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
 import logo from "../public/logo-yellowbg.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 
 function Navbar(props) {
@@ -9,14 +11,30 @@ function Navbar(props) {
     <nav>
       <div className="menu">
         <li>
+          <span>
+            <FontAwesomeIcon
+              className="nav-icons"
+              icon="fa-solid fa-bars"
+              style={{ color: "#f8fcfc" }}
+            />
+          </span>
           <Link to="/preferences">Preferences</Link>
         </li>
+
         <li id="logo">
           <Link to="/">
             <img src={logo} alt="logo" />
           </Link>
         </li>
+
         <li>
+          <span>
+            <FontAwesomeIcon
+              className="nav-icons"
+              icon="fa-solid fa-heart"
+              style={{ color: "#f8fcfc" }}
+            />
+          </span>
           <Link to="/saved">Saved</Link>
         </li>
       </div>
