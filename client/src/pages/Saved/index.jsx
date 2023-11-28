97ebdd84ddc94ff3443994542fe3a4cea4e6adc1
Wilson Chu <wilson1.chu@gmail.com>
2023-11-27 23:01:43 -0500

    display saved_jobs DB info on Saved Jobs route

--- client/src/pages/Saved/index.jsx
+++ client/src/pages/Saved/index.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./saved.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Saved(props) {
+  const [savedJobs, setSavedJobs] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get("/api/savedJobs")
+      .then((res) => {
+        setSavedJobs(res.data[0]);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <>
       <h2>My Saved Jobs</h2>
@@ -13,12 +28,16 @@ function Saved(props) {
             size="xl"
             className="check-applied"
           />
-          <h3>Job Title, Company Name</h3>
+          <h3>
+            {savedJobs.job_title}, {savedJobs.company}
+          </h3>
           <p>X</p>
         </div>
         <div className="bottom-saved-box">
           <span>Review Posting</span>
-          <span>Apply To Job</span>
+          <span>
+            <a href={savedJobs.website} target="_blank" rel="noopener noreferrer">Apply To Job</a>
+          </span>
         </div>
       </section>
     </>
