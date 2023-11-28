6a7c38a047cf4182aedaaae79840d434035996b8
Wilson Chu <wilson1.chu@gmail.com>
2023-11-27 19:28:26 -0500

    create basic skeleton for Saved Jobs, change default value of applied in DB schema to FALSE instead of NULL

--- client/src/pages/Saved/index.jsx
+++ client/src/pages/Saved/index.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React from "react";
+import "./saved.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Saved(props) {
   return (
     <>
-      <div>
-        <h2>Saved Jobs</h2>
-        <ul>
-        <li>
-          I'm a cool info box right here!
-        </li>
-        </ul>
-      </div>
+      <h2>My Saved Jobs</h2>
+      <section>
+        <div className="top-saved-box">
+          <FontAwesomeIcon
+            icon="fa-solid fa-check"
+            size="2xl"
+            className="check-applied"
+          />
+          <h3>Job Title, Company Name</h3>
+        </div>
+        <div className="bottom-saved-box">
+          <span>Review Posting</span>
+          <span>Apply To Job</span>
+        </div>
+      </section>
     </>
   );
 }
