549e19082feee3c610fdd6f648f8851ffaf89ae4
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-11-28 17:08:48 -0500

    put image in main job points component

--- client/src/pages/Home/MainJobPoints.jsx
+++ client/src/pages/Home/MainJobPoints.jsx
@@ -3,23 +3,27 @@ import React from 'react';
 const MainJobPoints = function (props) {
   return (
     <div className='main-points-container'>
-      <h2>
-        {props.data[props.job].job_title}
-      </h2>
-      <ul className="main-job-points">
-        <li>
-          <i className="fa-solid fa-briefcase"></i>
-          <p>{props.data[props.job].employer_name}</p>
-        </li>
-        <li>
-          <i className="fa-solid fa-location-dot"></i>
-          <p>{props.data[props.job].job_city}</p>
-        </li>
-        <li>
-          <i className="fa-regular fa-clock"></i>
-          <p>{props.data[props.job].job_employment_type}</p>
-        </li>
-      </ul>
+      {props.data[props.job].employer_logo && <div><img className="main-image" src={props.data[2].employer_logo} alt="employer logo" onClick={() => props.openModal()} /></div>}
+      {!props.data[props.job].employer_logo && <div><img className="main-image" src="https://redlakejobs.ca/wp-content/uploads/2020/10/employment.jpg" alt="jobs" onClick={() => props.openModal()} /></div>}
+      <div className='main-points-text-container'>
+        <h2>
+          {props.data[props.job].job_title}
+        </h2>
+        <ul className="main-job-points">
+          <li>
+            <i className="fa-solid fa-briefcase"></i>
+            <p>{props.data[props.job].employer_name}</p>
+          </li>
+          <li>
+            <i className="fa-solid fa-location-dot"></i>
+            <p>{props.data[props.job].job_city}</p>
+          </li>
+          <li>
+            <i className="fa-regular fa-clock"></i>
+            <p>{props.data[props.job].job_employment_type}</p>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 };
