057c61be878140e6fc6be34d3ff3c1adecfb3b1d
lorraineC26 <lorraine.quinyee@gmail.com>
2023-11-28 21:43:43 -0500

    adjust the syntax

--- client/src/api-data/apiData.js
+++ client/src/api-data/apiData.js
@@ -1,48 +1,48 @@
-
 import axios from "axios";
-
-function fetchData() {
-  const options = {
-    method: 'GET',
-    url: 'https://jsearch.p.rapidapi.com/search',
-    params: {
-      query: 'developer in california',
-      date_posted: 'all',
-      country: 'CA'
-    },
-    headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
-      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-    }
-  };
-
-
-
 
 // function fetchData() {
 //   const options = {
-//     method: 'GET',
-//     url: 'https://jsearch.p.rapidapi.com/search',
+//     method: "GET",
+//     url: "https://jsearch.p.rapidapi.com/search",
 //     params: {
-//       query: 'developer in california',
-//       date_posted: 'all',
-//       country: 'CA'
+//       query: "developer in california",
+//       date_posted: "all",
+//       country: "CA",
 //     },
 //     headers: {
-//       'X-RapidAPI-Key': 
-//       'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-//     }
+//       "X-RapidAPI-Key": "",
+//       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+//     },
 //   };
+// };
 
-//   return axios.request(options)
-//     .then(response => {
-//       console.log(response.data.data);
-//       return response.data;
-//     })
-//     .catch(error => {
-//       console.error(error);
-//       throw error;
-//     });
-// }
 
-// export default fetchData;
+
+
+function fetchData() {
+  const options = {
+    method: "GET",
+    url: "https://jsearch.p.rapidapi.com/search",
+    params: {
+      query: "developer in california",
+      date_posted: "all",
+      country: "CA",
+    },
+    headers: {
+      "X-RapidAPI-Key": "", // process.env.REACT_APP_RAPIDAPI_KEY
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+  };
+
+  return axios.request(options)
+    .then(response => {
+      console.log(response.data.data);
+      return response.data;
+    })
+    .catch(error => {
+      console.error(error);
+      throw error;
+    });
+}
+
+export default fetchData;
