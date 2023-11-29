a90f4b0b0d539b018b21572413b68e7fbbe9565c
Wilson Chu <wilson1.chu@gmail.com>
2023-11-28 20:36:03 -0500

    testing api retrieval

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import data from "../../mockdata/data"
+// import data from "../../mockdata/data"
+import fetchData from '../../api-data/data'; 
 import MainJobPoints from './MainJobPoints';
 import JobDetailsModal from './JobDetailsModal';
 import ActionButtons from './ActionButtons';
 import "./Home.scss"
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Home = function(props) {
+
+  // useEffect(() => {
+    
+  //   const apiData=searchData;
+  //   console.log(apiData);
+  // }, []);
+
+  fetchData();
 
   const [job, setJob] = useState(0)
   const [modal, setModal] = useState(false)
@@ -25,7 +34,7 @@ const Home = function(props) {
 
   return (
       <div>
-        {data.data[job].employer_logo && <div><img className="main-image" src={data.data[2].employer_logo} alt="employer logo" onClick={() => openModal()}/></div>}
+        {data.data[job].employer_logo && <div><img className="main-image" src={data.data[job].employer_logo} alt="employer logo" onClick={() => openModal()}/></div>}
         {!data.data[job].employer_logo && <div><img className="main-image" src="https://redlakejobs.ca/wp-content/uploads/2020/10/employment.jpg" alt="jobs" onClick={() => openModal()}/></div>}
         <MainJobPoints data={data.data} job={job}/>
         <ActionButtons data={data.data} job={job} nextJob={nextJob}/>
