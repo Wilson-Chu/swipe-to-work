e5b1f36da385494c9642f92002c3864c8a549d26
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-11-30 11:21:46 -0500

    lift state related to jobs and modal to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,19 +6,22 @@ import Home from "./pages/Home";
 import Preferences from "./pages/Preferences";
 import Saved from "./pages/Saved";
 import Navbar from "./components/Navbar";
-
+import useApplicationData from './hooks/useApplicationData';
 import { faCheck, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons'
 library.add(fas, faCheck, faCircleXmark);
 
 export default function App() {
+
+  const {jobs, jobIndex, modal, openModal, closeModal, nextJob} = useApplicationData();
+
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home jobs={jobs} jobIndex={jobIndex} modal={modal} openModal={openModal} closeModal={closeModal} nextJob={nextJob}/>} />
           {/* <Route path="/account" element={<Account />} /> */}
           <Route path="/preferences" element={<Preferences />} />
           <Route path="/saved" element={<Saved />} />
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -2,24 +2,15 @@ import React from 'react';
 import MainJobPoints from './MainJobPoints';
 import JobDetailsModal from './JobDetailsModal';
 import ActionButtons from './ActionButtons';
-import useApplicationData from '../../hooks/useApplicationData';
 import "./Home.scss"
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-
 
 const Home = function(props) {
 
-  const {jobs, jobIndex, modal, openModal, closeModal, nextJob} = useApplicationData();
-  console.log("JOBS", jobs)
-  console.log("INDEX", jobIndex)
- 
-
   return (
       <div className='home'>
-        <MainJobPoints jobs={jobs} jobIndex={jobIndex}  openModal={openModal}/>
-        <ActionButtons jobs={jobs} jobIndex={jobIndex} nextJob={nextJob}/>
-        {!!modal && <JobDetailsModal jobs={jobs} closeModal={closeModal} jobIndex={jobIndex}/>}
+        <MainJobPoints jobs={props.jobs} jobIndex={props.jobIndex}  openModal={props.openModal}/>
+        <ActionButtons jobs={props.jobs} jobIndex={props.jobIndex} nextJob={props.nextJob}/>
+        {!!props.modal && <JobDetailsModal jobs={props.jobs} closeModal={props.closeModal} jobIndex={props.jobIndex}/>}
       </div>
   );
 }
