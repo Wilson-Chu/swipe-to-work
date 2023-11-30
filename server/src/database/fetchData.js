b0a12688dd94082911178f078f416b58693c16ea
Wilson Chu <wilson1.chu@gmail.com>
2023-11-29 22:42:35 -0500

    refactor callbacks into async/await block

--- server/src/database/fetchData.js
+++ server/src/database/fetchData.js
@@ -2,19 +2,31 @@ const axios = require("axios");
 const { getPref } = require("../database/prefHelpers");
 
 const fetchData = async function() {
-    
-    const data = await getPref(1) // 1 is sample userID
+  try {
+    const data = await getPref(1); // 1 is a sample userID
     console.log(data);
 
-    let {id, job_title, company, city, province, min_salary, job_type, is_remote, no_experience_required, min_education_level, user_id} = data;
+    let {
+      id,
+      job_title,
+      company,
+      city,
+      province,
+      min_salary,
+      job_type,
+      is_remote,
+      no_experience_required,
+      min_education_level,
+      user_id,
+    } = data;
 
-    job_title ? job_title : 'Web developer';
-    city ? city: 'Toronto';
-    province ? province: 'ON';
-    no_experience_required ? 'no_experience' : 'more_than_3_years_experience';
-    min_salary ? `minimum salary of ${min_salary}` : null;
-    min_education_level ? `${min_education_level} required` : null;
- 
+    // Set default values if the variables are undefined
+    job_title = job_title || 'Web developer';
+    city = city || 'Toronto';
+    province = province || 'ON';
+    no_experience_required = no_experience_required ? 'no_experience' : 'more_than_3_years_experience';
+    min_salary = min_salary ? `minimum salary of ${min_salary}` : null;
+    min_education_level = min_education_level ? `${min_education_level} required` : null;
 
     const options = {
       method: "GET",
@@ -28,21 +40,18 @@ const fetchData = async function() {
         // job_requirements: `${no_experience_required}`,
         remote_jobs_only: is_remote,
         employment_types: `${job_type}`,
-        
-
       },
       headers: {
         "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
         "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
       },
     };
-    return axios.request(options)
-      .then(response => {
-        return response.data;
-      })
-      .catch(error => {
-        throw error;
-      });
-  }
 
-module.exports = {fetchData};
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { fetchData };
