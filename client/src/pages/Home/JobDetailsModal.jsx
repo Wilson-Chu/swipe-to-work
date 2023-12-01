5f7e0fe50b441e5743d7ca7892fc4618d8f019a8
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-12-01 17:52:15 -0500

    update tags

--- client/src/pages/Home/JobDetailsModal.jsx
+++ client/src/pages/Home/JobDetailsModal.jsx
@@ -1,44 +1,47 @@
 import React from "react";
 import closeSymbol from "../../assets/closeSymbol.svg"
 import { educationList, descWithLineBreaks } from "./homeHelpers";
+import "./JobDetailsModal.scss"
 
 const JobDetailsModal = function (props) {
   return (
-    <div className="job-details-modal">
+    <div className="job-details-modal-container">
       <button className="job-details-modal__close-button">
         <img src={closeSymbol} alt="close symbol" onClick={props.closeModal} />
       </button>
       <h2>{props.jobs[props.jobIndex].job_title}</h2>
-      <ul className="modal-job-details">
-        <li>
-          Company: {props.jobs[props.jobIndex].employer_name}
-        </li>
-        <li>
-          City: {props.jobs[props.jobIndex].job_city}
-        </li>
-        <li>
-          Province: {props.jobs[props.jobIndex].job_state}
-        </li>
-        <li>
-          Mininum salary: {props.jobs[props.jobIndex].job_min_salary ? props.jobs[props.jobIndex].job_min_salary : "N/A"}
-        </li>
-        <li>
-          Job type: {props.jobs[props.jobIndex].job_employment_type}
-        </li>
-        <li>
-          Job is remote? {(props.jobs[props.jobIndex].job_is_remote).toString()}
-        </li>
-        <li>
-          Job description: {descWithLineBreaks(props.jobs[props.jobIndex].job_description)}
-        </li>
-        <li>
-          Posted at: {props.jobs[props.jobIndex].job_posted_at_datetime_utc.split("T").shift()}
-        </li>
-          Education required: {educationList(props.jobs)}
-        <li>
-          No experience required? {(props.jobs[props.jobIndex].job_required_experience.no_experience_required).toString()}
-        </li>
-      </ul>
+      <div className="modal-job-details">
+        <section>
+          <span>Company: </span>{props.jobs[props.jobIndex].employer_name}
+        </section>
+        <section>
+          <span>City:</span> {props.jobs[props.jobIndex].job_city}
+        </section>
+        <section>
+          <span>Province:</span> {props.jobs[props.jobIndex].job_state}
+        </section>
+        <section>
+          <span>Mininum salary:</span> {props.jobs[props.jobIndex].job_min_salary ? props.jobs[props.jobIndex].job_min_salary : "N/A"}
+        </section>
+        <section>
+          <span>Job type:</span> {props.jobs[props.jobIndex].job_employment_type}
+        </section>
+        <section>
+          <span>Job is remote?</span> {(props.jobs[props.jobIndex].job_is_remote).toString()}
+        </section>
+        <section>
+          <span className="job-desc">Job description:</span> {descWithLineBreaks(props.jobs[props.jobIndex].job_description)}
+        </section>
+        <section>
+          <span>Posted at:</span> {props.jobs[props.jobIndex].job_posted_at_datetime_utc.split("T").shift()}
+        </section>
+        <section>
+          <span>Education required:</span> {educationList(props.jobs[props.jobIndex].job_required_education)}
+        </section>
+        <section>
+          <span>No experience required?</span> {(props.jobs[props.jobIndex].job_required_experience.no_experience_required).toString()}
+        </section>
+      </div>
     </div>
   )
 };
