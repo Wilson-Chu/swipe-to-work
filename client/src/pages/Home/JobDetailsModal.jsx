d0fbfe43015cd03624b9821b39d4427e299e91d6
lorraineC26 <lorraine.quinyee@gmail.com>
2023-12-01 14:31:05 -0500

    adjust homepage layout; add jobtype helperfx

--- client/src/pages/Home/JobDetailsModal.jsx
+++ client/src/pages/Home/JobDetailsModal.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import closeSymbol from "../../assets/closeSymbol.svg"
-import { educationList, descWithLineBreaks } from "./homeHelpers";
+import {
+  educationList,
+  descWithLineBreaks,
+  jobTypeFormatter,
+} from "./homeHelpers";
 
 const JobDetailsModal = function (props) {
   return (
@@ -10,37 +14,42 @@ const JobDetailsModal = function (props) {
       </button>
       <h2>{props.jobs[props.jobIndex].job_title}</h2>
       <ul className="modal-job-details">
+        <li>Company: {props.jobs[props.jobIndex].employer_name}</li>
+        <li>City: {props.jobs[props.jobIndex].job_city}</li>
+        <li>Province: {props.jobs[props.jobIndex].job_state}</li>
         <li>
-          Company: {props.jobs[props.jobIndex].employer_name}
+          Mininum salary:{" "}
+          {props.jobs[props.jobIndex].job_min_salary
+            ? props.jobs[props.jobIndex].job_min_salary
+            : "N/A"}
         </li>
         <li>
-          City: {props.jobs[props.jobIndex].job_city}
+          Job type:{" "}
+          {jobTypeFormatter(props.jobs[props.jobIndex].job_employment_type)}
         </li>
         <li>
-          Province: {props.jobs[props.jobIndex].job_state}
+          Job is remote? {props.jobs[props.jobIndex].job_is_remote.toString()}
         </li>
         <li>
-          Mininum salary: {props.jobs[props.jobIndex].job_min_salary ? props.jobs[props.jobIndex].job_min_salary : "N/A"}
+          Job description:{" "}
+          {descWithLineBreaks(props.jobs[props.jobIndex].job_description)}
         </li>
         <li>
-          Job type: {props.jobs[props.jobIndex].job_employment_type}
+          Posted at:{" "}
+          {props.jobs[props.jobIndex].job_posted_at_datetime_utc
+            .split("T")
+            .shift()}
         </li>
+        Education required: {educationList(props.jobs)}
         <li>
-          Job is remote? {(props.jobs[props.jobIndex].job_is_remote).toString()}
-        </li>
-        <li>
-          Job description: {descWithLineBreaks(props.jobs[props.jobIndex].job_description)}
-        </li>
-        <li>
-          Posted at: {props.jobs[props.jobIndex].job_posted_at_datetime_utc.split("T").shift()}
-        </li>
-          Education required: {educationList(props.jobs)}
-        <li>
-          No experience required? {(props.jobs[props.jobIndex].job_required_experience.no_experience_required).toString()}
+          No experience required?{" "}
+          {props.jobs[
+            props.jobIndex
+          ].job_required_experience.no_experience_required.toString()}
         </li>
       </ul>
     </div>
-  )
+  );
 };
 
 export default JobDetailsModal;
--- client/src/pages/Home/MainJobPoints.jsx
+++ client/src/pages/Home/MainJobPoints.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { jobTypeFormatter } from './homeHelpers'
 
 const MainJobPoints = function (props) {
 
@@ -12,47 +13,58 @@ const MainJobPoints = function (props) {
   return (
     props.jobs.length > 0 && (
       <>
-      {!!props.loading && <h2>Finding your perfect job!</h2>}
-      {!props.loading && <div className={`main-points-container ${isJobSaved ? "saved" : ""} ${isJobPassed ? "passed" : ""}`}>
-        {props.jobs[props.jobIndex].employer_logo && (
-          <div className="main-img-container">
-            <img
-              className="main-image"
-              src={props.jobs[props.jobIndex].employer_logo}
-              alt="employer logo"
-              onClick={() => props.openModal()}
-              onError={handleImageError}
-            />
+        {!!props.loading && <h2>Finding your perfect job!</h2>}
+        {!props.loading && (
+          <div
+            className={`main-points-container ${isJobSaved ? "saved" : ""} ${
+              isJobPassed ? "passed" : ""
+            }`}
+          >
+            {props.jobs[props.jobIndex].employer_logo && (
+              <div className="main-img-container">
+                <img
+                  className="main-image"
+                  src={props.jobs[props.jobIndex].employer_logo}
+                  alt="employer logo"
+                  onClick={() => props.openModal()}
+                  onError={handleImageError}
+                />
+              </div>
+            )}
+            {!props.jobs[props.jobIndex].employer_logo && (
+              <div className="main-img-container">
+                <img
+                  className="main-image"
+                  src="https://redlakejobs.ca/wp-content/uploads/2020/10/employment.jpg"
+                  alt="jobs"
+                  onClick={() => props.openModal()}
+                />
+              </div>
+            )}
+            <div className="main-points-text-container">
+              <h2>{props.jobs[props.jobIndex].job_title}</h2>
+              <ul className="main-job-points">
+                <li>
+                  <i className="fa-solid fa-briefcase"></i>
+                  <p>{props.jobs[props.jobIndex].employer_name}</p>
+                </li>
+                <li>
+                  <i className="fa-solid fa-location-dot"></i>
+                  <p>{props.jobs[props.jobIndex].job_city}</p>
+                </li>
+                <li>
+                  <i className="fa-regular fa-clock"></i>
+                  <p>
+                    {
+                      jobTypeFormatter(props.jobs[props.jobIndex]
+                        .job_employment_type)
+                    }
+                  </p>
+                </li>
+              </ul>
+            </div>
           </div>
         )}
-        {!props.jobs[props.jobIndex].employer_logo && (
-          <div className="main-img-container">
-            <img
-              className="main-image"
-              src="https://redlakejobs.ca/wp-content/uploads/2020/10/employment.jpg"
-              alt="jobs"
-              onClick={() => props.openModal()}
-            />
-          </div>
-        )}
-        <div className="main-points-text-container">
-          <h2>{props.jobs[props.jobIndex].job_title}</h2>
-          <ul className="main-job-points">
-            <li>
-              <i className="fa-solid fa-briefcase"></i>
-              <p>{props.jobs[props.jobIndex].employer_name}</p>
-            </li>
-            <li>
-              <i className="fa-solid fa-location-dot"></i>
-              <p>{props.jobs[props.jobIndex].job_city}</p>
-            </li>
-            <li>
-              <i className="fa-regular fa-clock"></i>
-              <p>{props.jobs[props.jobIndex].job_employment_type}</p>
-            </li>
-          </ul>
-        </div>
-      </div>}
       </>
     )
   );
