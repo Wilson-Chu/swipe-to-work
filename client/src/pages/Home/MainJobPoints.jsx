6984711c70ed5c2132b560b8d14e145c1defaae1
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-12-01 17:52:51 -0500

    add new message if api returns no data

--- client/src/pages/Home/MainJobPoints.jsx
+++ client/src/pages/Home/MainJobPoints.jsx
@@ -8,51 +8,57 @@ const MainJobPoints = function (props) {
     // Replace any broken image with the default image
     event.target.src = "https://redlakejobs.ca/wp-content/uploads/2020/10/employment.jpg";
   };
-
+  
   return (
     props.jobs.length > 0 && (
       <>
-      {!!props.loading && <h2>Finding your perfect job!</h2>}
-      {!props.loading && <div className={`main-points-container ${isJobSaved ? "saved" : ""} ${isJobPassed ? "passed" : ""}`}>
-        {props.jobs[props.jobIndex].employer_logo && (
-          <div className="main-img-container">
-            <img
-              className="main-image"
-              src={props.jobs[props.jobIndex].employer_logo}
-              alt="employer logo"
-              onClick={() => props.openModal()}
-              onError={handleImageError}
-            />
+        {!!props.loading && <h2 className='loading-text'>Finding your perfect job!</h2>}
+        {!props.loading && <div className={`main-points-container ${isJobSaved ? "saved" : ""} ${isJobPassed ? "passed" : ""}`}>
+          {props.jobs[props.jobIndex].employer_logo && (
+            <div className="main-img-container">
+              <img
+                className="main-image"
+                src={props.jobs[props.jobIndex].employer_logo}
+                alt="employer logo"
+                onClick={() => props.openModal()}
+                onError={handleImageError}
+              />
+            </div>
+          )}
+          {!props.jobs[props.jobIndex].employer_logo && (
+            <div className="main-img-container">
+              <img
+                className="main-image"
+                src="https://redlakejobs.ca/wp-content/uploads/2020/10/employment.jpg"
+                alt="jobs"
+                onClick={() => props.openModal()}
+              />
+            </div>
+          )}
+          <div className="main-points-text-container">
+            <h2>{props.jobs[props.jobIndex].job_title}</h2>
+            <ul className="main-job-points">
+              <li>
+                <i className="fa-solid fa-briefcase"></i>
+                <p>{props.jobs[props.jobIndex].employer_name}</p>
+              </li>
+              <li>
+                <i className="fa-solid fa-location-dot"></i>
+                <p>{props.jobs[props.jobIndex].job_city}</p>
+              </li>
+              <li>
+                <i className="fa-regular fa-clock"></i>
+                <p>{props.jobs[props.jobIndex].job_employment_type}</p>
+              </li>
+            </ul>
           </div>
-        )}
-        {!props.jobs[props.jobIndex].employer_logo && (
-          <div className="main-img-container">
-            <img
-              className="main-image"
-              src="https://redlakejobs.ca/wp-content/uploads/2020/10/employment.jpg"
-              alt="jobs"
-              onClick={() => props.openModal()}
-            />
-          </div>
-        )}
-        <div className="main-points-text-container">
-          <h2>{props.jobs[props.jobIndex].job_title}</h2>
-          <ul className="main-job-points">
-            <li>
-              <i className="fa-solid fa-briefcase"></i>
-              <p>{props.jobs[props.jobIndex].employer_name}</p>
-            </li>
-            <li>
-              <i className="fa-solid fa-location-dot"></i>
-              <p>{props.jobs[props.jobIndex].job_city}</p>
-            </li>
-            <li>
-              <i className="fa-regular fa-clock"></i>
-              <p>{props.jobs[props.jobIndex].job_employment_type}</p>
-            </li>
-          </ul>
-        </div>
-      </div>}
+        </div>}
+      </>
+    ) ||
+    props.jobs.length === 0 && (
+      <>
+        {!!props.loading && <h2 className='loading-text'>Finding your perfect job!</h2>}
+        {!props.loading && <h2 className='loading-text'>Oops, please expand your search!</h2>}
       </>
     )
   );
