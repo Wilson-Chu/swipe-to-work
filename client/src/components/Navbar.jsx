665f17b0fcde23365d5a19dc3aeff24cb9a48869
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-12-02 16:22:37 -0500

    links only show if user is logged in

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,56 +3,66 @@ import { Link } from "react-router-dom";
 import "./Navbar.scss";
 import logo from "../public/logo-yellowbg.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useAuth0 } from "@auth0/auth0-react";
 
 
 
 function Navbar(props) {
+  const { logout, isAuthenticated } = useAuth0();
+  const loginURL = import.meta.env.VITE_CLIENT_LOGIN_URL;
   return (
     <nav>
       <div className="menu">
-        <li id="logo">
-          <Link to="/">
-            <img src={logo} alt="logo" />
-          </Link>
-        </li>
-
-        <li>
-          <span>
-            <FontAwesomeIcon
-              className="nav-icons"
-              icon="fa-solid fa-bars"
-              style={{ color: "#f8fcfc" }}
-            />
-          </span>
-          <Link to="/preferences">Preferences</Link>
-        </li>
+        {isAuthenticated ?
+          <li id="logo">
+            <Link to="/">
+              <img src={logo} alt="logo" />
+            </Link>
+          </li> :
+          <li id="logo">
+              <img src={logo} alt="logo" />
+          </li>}
+        {isAuthenticated &&
+          <li>
+            <span>
+              <FontAwesomeIcon
+                className="nav-icons"
+                icon="fa-solid fa-bars"
+                style={{ color: "#f8fcfc" }}
+              />
+            </span>
+            <Link to="/preferences">Preferences</Link>
+          </li>}
 
         {/* <li id="logo">
           <Link to="/">
             <img src={logo} alt="logo" />
           </Link>
         </li> */}
+        {isAuthenticated &&
+          <li>
+            <span>
+              <FontAwesomeIcon
+                className="nav-icons"
+                icon="fa-solid fa-heart"
+                style={{ color: "#f8fcfc" }}
+              />
+            </span>
+            <Link to="/saved">Saved Jobs</Link>
+          </li>}
 
-        <li>
-          <span>
-            <FontAwesomeIcon
-              className="nav-icons"
-              icon="fa-solid fa-heart"
-              style={{ color: "#f8fcfc" }}
-            />
-          </span>
-          <Link to="/saved">Saved Jobs</Link>
-        </li>
-
-        <li>
-          <span>
-            <FontAwesomeIcon
-              icon="fa-solid fa-user"
-              style={{ color: "#f8fcfc" }}
-            />
-          </span>
-          <Link to="/login">Logout</Link>
-        </li>
+        {isAuthenticated &&
+          <li>
+            <span>
+              <FontAwesomeIcon
+                icon="fa-solid fa-user"
+                style={{ color: "#f8fcfc" }}
+              />
+            </span>
+            <span onClick={() => logout({ returnTo: loginURL })}>
+              Log out
+            </span>
+          </li>}
       </div>
     </nav>
   );
