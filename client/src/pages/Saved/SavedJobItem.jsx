171549ffadec51e7747d4d9e4833ce7184e3f8dc
Wilson Chu <wilson1.chu@gmail.com>
2023-12-02 20:44:34 -0500

    fix applied toggle info being sent to DB

--- client/src/pages/Saved/SavedJobItem.jsx
+++ client/src/pages/Saved/SavedJobItem.jsx
@@ -3,9 +3,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import SavedJobModal from "./SavedJobModal";
 import { useAppliedJobsContext } from "../../providers/AppliedJobsProvider";
 
-function SavedJobItem({ id, job_title, company, website, deleteSavedJob, updateSavedJobMarker, job, modal, applied}) {
+function SavedJobItem({
+  id,
+  job_title,
+  company,
+  website,
+  deleteSavedJob,
+  updateSavedJobMarker,
+  job,
+  modal,
+  applied,
+}) {
   const [appliedState, setAppliedState] = useState(!!applied);
-  
+
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [oneSavedJob, setOneSavedJob] = useState({});
   // slideout animation
@@ -13,15 +23,15 @@ function SavedJobItem({ id, job_title, company, website, deleteSavedJob, updateS
 
   const { appliedJobs, updateAppliedJobs } = useAppliedJobsContext();
 
-  const removeSavedJob = function(id) {
+  const removeSavedJob = function (id) {
     //for slideout animation
     setIsDeleted(true);
 
     deleteSavedJob(id);
 
     setTimeout(() => {
-      setIsDeleted(false)
-    }, 1000)
+      setIsDeleted(false);
+    }, 1000);
   };
 
   const closeModal = () => {
@@ -38,38 +48,43 @@ function SavedJobItem({ id, job_title, company, website, deleteSavedJob, updateS
   };
 
   const handleAppliedToggle = async () => {
-    setAppliedState(prevState => !prevState);
+    setAppliedState((prevState) => !prevState);
     // Toggle the 'applied' value if clicking on Check Mark icon
     const updatedData = {
-      applied: !appliedState, // DB NOT BEING UPDATED HERE FOR SOME REASON?
+      applied: appliedState ? false : true, // DB NOT BEING UPDATED HERE FOR SOME REASON?
     };
-    
-    console.log("Applied before update: ", applied);
+
+    // console.log("Applied before update: ", applied);
 
     try {
-      let updatedAppliedToggleValue = await updateSavedJobMarker(id, updatedData);
-      console.log(`Saved job ${id} updated successfully. Applied toggle: ${updatedAppliedToggleValue}`);
-      
+      let updatedAppliedToggleValue = await updateSavedJobMarker(
+        id,
+        updatedData
+      );
+      console.log(
+        `Saved job ${id} updated successfully. Applied toggle: ${updatedAppliedToggleValue}`
+      );
     } catch (error) {
       console.error("Error updating saved job:", error);
-      
     }
   };
 
   const handleApplyToJob = async () => {
+    event.preventDefault();
+
     setAppliedState(true);
     // Always set applied to true if Apply to Job link is clicked
     const updatedData = {
       applied: true,
     };
-  
+
     try {
       let updatedAppliedValue = await updateSavedJobMarker(id, updatedData);
-      console.log(`Saved job ${id} updated successfully. Applied: ${updatedAppliedValue}`);
-      
+      console.log(
+        `Saved job ${id} updated successfully. Applied: ${updatedAppliedValue}`
+      );
     } catch (error) {
       console.error("Error updating saved job:", error);
-      
     }
   };
 
@@ -85,13 +100,16 @@ function SavedJobItem({ id, job_title, company, website, deleteSavedJob, updateS
 
   return (
     <>
-      <section className={`saved-job-item ${isDeleted ? "deleted": ""}`}>
+      <section className={`saved-job-item ${isDeleted ? "deleted" : ""}`}>
         <div className="top-saved-box">
           <FontAwesomeIcon
             icon="fa-solid fa-check"
             size="xl"
-            className={`check-applied ${appliedState ? "mark-applied": ""}`}
-            onClick={()=>{handleAppliedToggle(); handleUpdateAppliedJobs();}}
+            className={`check-applied ${appliedState ? "mark-applied" : ""}`}
+            onClick={() => {
+              handleAppliedToggle();
+              handleUpdateAppliedJobs();
+            }}
           />
           <h3>
             {job_title}, {company}
@@ -103,18 +121,33 @@ function SavedJobItem({ id, job_title, company, website, deleteSavedJob, updateS
           />
         </div>
         <div className="bottom-saved-box">
-          <span className = "review-link" onClick={() => reviewPosting()}>
+          <span className="review-link" onClick={() => reviewPosting()}>
             Review Posting
           </span>
-          <span onClick={()=>{handleApplyToJob(); }}>
-            <a href={website} target="_blank" rel="noopener noreferrer" className="apply-link">
-              <span>Apply To Job  </span>
+          <span
+            onClick={() => {
+              handleApplyToJob();
+            }}
+          >
+            <a
+              href={website}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="apply-link"
+            >
+              <span>Apply To Job </span>
               <FontAwesomeIcon icon="fa-solid fa-arrow-up-right-from-square" />
             </a>
           </span>
         </div>
       </section>
-          {isModalOpen && <SavedJobModal job={oneSavedJob} closeModal={closeModal} modal={modal}/>}
+      {isModalOpen && (
+        <SavedJobModal
+          job={oneSavedJob}
+          closeModal={closeModal}
+          modal={modal}
+        />
+      )}
       <br />
     </>
   );
